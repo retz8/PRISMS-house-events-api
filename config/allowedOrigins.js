2c578ed681f068a0c00b6da807f80fb6faa15b3b
retz8 <ekkicb71@gmail.com>
2023-06-06 22:14:28 -0400

    dev env

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://prisms-house-events-admin.onrender.com"];
+const allowedOrigins = [
+  "https://prisms-house-events-admin.onrender.com",
+  "http://localhost:3000",
+];
 
 module.exports = allowedOrigins;
